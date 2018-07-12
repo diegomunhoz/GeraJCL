cd4935db0495501d04d25c19b5813ef524f5d354
Diego Munhoz <diego.fatec@hotmail.com>
2018-07-12 16:25:07 -0300

    Projeto GeraJCL

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import model.GeraJclQuebraLote;
+
+public class Main {
+
+	public static void main(String[] args) {
+		GeraJclQuebraLote quebraLote = new GeraJclQuebraLote();
+		quebraLote.processar();
+
+		//GeralJclExpurgo e = new GeralJclExpurgo();
+		//e.processar();
+		
+		//GeraJclTU working = new GeraJclTU();
+		//working.processar();
+		
+	}
+
+}
